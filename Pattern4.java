e5ccbf178d996f49a4a5f68c49f5b4bc0c12a143
pankajsharma7352 <pankajsharma735298@gmail.com>
2023-07-30 19:27:08 +0530

    first commit

---
+++ Pattern4.java
@@ -0,0 +1,13 @@
+public class Pattern4 {
+    public static void main(String[] args) {
+        
+        int num =5;
+        for(int i=5; i>=1; i--){
+            for(int j=1; j<=5; j++){
+
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
