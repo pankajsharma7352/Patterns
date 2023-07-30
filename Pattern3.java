e1c606cdc1dd151a832bab1f65d4a70f1d6960ff
pankajsharma7352 <pankajsharma735298@gmail.com>
2023-07-30 19:08:59 +0530

    first commit

---
+++ Pattern3.java
@@ -0,0 +1,13 @@
+public class Pattern3 {
+    
+    public static void main(String[] args) {
+        
+        int num =5;
+        for(int i=1; i<=num; i++){
+            for(int j=1; j<=num; j++){
+                System.out.print(i+""+j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
